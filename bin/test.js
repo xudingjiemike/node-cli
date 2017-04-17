366e78d5bd49dd1937106e4767ab97f576133fe1
xudj <xudj@xudjdeMacBook-Air.local>
2017-04-17 22:25:31 +0800

    exec 为异步

--- bin/test.js
+++ bin/test.js
@@ -1,16 +1,9 @@
-const spawn = require('child_process').spawn
-
-const result = spawn('ls', ['-lh', '/usr']);
-
-
-result.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
-
-result.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-});
-
-result.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+const exec = require('child_process').exec;
+exec('pwd', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
 });
