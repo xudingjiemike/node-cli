09b9cdff9c00cccb120ec5e05eb99997932349d4
xudj <xudj@xudjdeMacBook-Air.local>
2017-04-18 23:42:28 +0800

    有参数的命令处理

--- bin/test.js
+++ bin/test.js
@@ -1,9 +1,19 @@
-const exec = require('child_process').exec;
-exec('pwd', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
+const spawn = require('child_process').spawn
+
+const ls = spawn('just',['watch'])
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
 });
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+ls.on('error',(data) => {
+    console.log(data);
+})
